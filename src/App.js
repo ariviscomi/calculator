22b0fa203fb7ea7a54a9859fc80a2b66f64da307
ariviscomi <arielviscomi.mail@gmail.com>
2023-02-22 11:38:06 -0300

    styles

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import './styles/css/style.css';
 function App() {
 	return (
 		<main id='calcContainer'>
-			<textarea rows="3" cols="15" />
+			<textarea className='textarea' rows="3" cols="15" />
 			<section id='numContainer'>
-				<span>0</span>
-				<span>1</span>
-				<span>2</span>
-				<span>3</span>
-				<span>4</span>
-				<span>5</span>
-				<span>6</span>
-				<span>7</span>
-				<span>8</span>
 				<span>9</span>
+				<span>8</span>
+				<span>7</span>
+				<span>6</span>
+				<span>5</span>
+				<span>4</span>
+				<span>3</span>
+				<span>2</span>
+				<span>1</span>
+				<span>0</span>
+				<span>,</span>
 			</section>
 			<section id='controlContainer'>
-				<span>  </span>
-				<span>5</span>
-				<span>6</span>
-				<span>7</span>
-				<span>8</span>
-				<span>9</span>
+				<span>←</span>
+				<span>÷</span>
+				<span>x</span>
+				<span>-</span>
+				<span>+</span>
+				<span>=</span>
 			</section>
 		</main>
 	);
